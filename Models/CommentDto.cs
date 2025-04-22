5c7ef66a7ad4a432fb16d3c6e08af10ab9db3e28
yasmine2512 <155321893+yasmine2512@users.noreply.github.com>
2025-04-22 22:27:00 +0100

    Update CommentDto.cs

--- Models/CommentDto.cs
+++ Models/CommentDto.cs
@@ -1,25 +1,10 @@
-using System;
-using System.Text.Json.Serialization;
-using System.ComponentModel.DataAnnotations.Schema;
-using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
-public class Comment
+public class CommentDto
 {
     public int CommentID { get; set; }
-
-  
     public int UserID { get; set; }
-
-
-    public int MemoireID { get; set; }
-
-
     public string Text { get; set; }
+    public DateTime Date { get; set; }
 
-    public DateTime Date { get; set; } = DateTime.Now;
-
-    // Navigation
-    [JsonIgnore]
-    public User User { get; set; }
-    [JsonIgnore]
- public Memoire Memoire { get; set; }
+    public string FullName { get; set; }
+    public string PicturePath { get; set; }
 }

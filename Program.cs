1fa23fd34ad015efa5f50fe2a07c2aaad86fd159
yasmine2512 <155321893+yasmine2512@users.noreply.github.com>
2025-05-23 17:27:31 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -2,25 +2,18 @@ using Microsoft.EntityFrameworkCore;
 using MyBlazorApp.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.FileProviders;
-
 var builder = WebApplication.CreateBuilder(args);
 
-// Database context
+
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseMySql(builder.Configuration.GetConnectionString("DefaultConnection"),
     new MySqlServerVersion(new Version(8, 0, 34)))
 );
-
-// Add configuration
 builder.Services.AddSingleton<IConfiguration>(builder.Configuration);
-
 //  Add HttpClient (IMPORTANT for ThesisSummarizerService)
-builder.Services.AddHttpClient();
+ builder.Services.AddHttpClient();
 
-// ThesisSummarizerService
-builder.Services.AddScoped<ThesisSummarizerService>();
 
-// CORS policy
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowFrontend",
@@ -31,39 +24,23 @@ builder.Services.AddCors(options =>
                   .AllowAnyMethod();
         });
 });
-
-// Controllers
 builder.Services.AddControllers();
-
-// Swagger
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
-// Kestrel server settings
 builder.WebHost.ConfigureKestrel(serverOptions =>
 {
     serverOptions.ListenLocalhost(5110); // HTTP
-});
 
-// Validation behavior
+});
 builder.Services.Configure<ApiBehaviorOptions>(options =>
 {
     options.SuppressModelStateInvalidFilter = false;
 });
-
-// Other services
 builder.Services.AddScoped<UserState>();
 
 var app = builder.Build();
 
-// Swagger UI
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
 
-// Static files
 app.UseStaticFiles();
 app.UseStaticFiles(new StaticFileOptions
 {
@@ -72,15 +49,13 @@ app.UseStaticFiles(new StaticFileOptions
     RequestPath = "/profile_pictures"
 });
 
-// Middlewares
+// app.UseHttpsRedirection();
 app.UseCors("AllowFrontend");
 app.UseRouting();
 app.UseAuthorization();
-
 app.UseEndpoints(endpoints =>
-{
-    endpoints.MapControllers();
-});
-
-// Run the app
+    {
+        endpoints.MapControllers();  
+    });
+//app.MapControllers();
 app.Run();
